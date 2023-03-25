707fb25a7c08bd546839f9a6a60b73d1a7083483
ssoprych <sebastian.soprych@gmail.com>
2023-03-25 15:22:35 +0100

    grid based movement

--- Assets/_project/Scripts/Coin.cs
+++ Assets/_project/Scripts/Coin.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Coin : MonoBehaviour
 {
+    public Tilemap tileMap;
+    public TileBase baseTile;
+    private Vector3Int position;
+    private Vector3Int positionTile;
+    private int _positionX;
+    private int _positionY;
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector3Int gridPosition = tileMap.WorldToCell(mousePosition);
+
+            //TileBase clickedTile = tileMap.GetTile(gridPosition);
+           // tileMap.SetTile(gridPosition, baseTile);
+           // print(clickedTile);
+       // }
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PagMan"))
         {
+            Vector2 playerPosition = Camera.main.ScreenToWorldPoint(collision.gameObject.transform.position);
+            Vector3Int gridPosition = tileMap.WorldToCell(playerPosition);
+            tileMap.SetTile(gridPosition, baseTile);
             GameManager.Instance.Score += 20;
-            Destroy(gameObject);
         }
     }
 }
--- Assets/_project/Scripts/PagMan.cs
+++ Assets/_project/Scripts/PagMan.cs
@@ -6,42 +6,53 @@ public class PagMan : MonoBehaviour
 {
     public Rigidbody2D Rb;
     public Animator Anim;
-    public float Speed = 3;
+    public Transform movePoint;
+    public float Speed = 3f;
+    public LayerMask whatStopsMovement;
 
     private void Start()
     {
+        movePoint.parent = null;
         Rb = GetComponent<Rigidbody2D>();
         Anim = GetComponent<Animator>();
-        
+
     }
 
     public void Move()
     {
+        Rb.position = Vector3.MoveTowards(Rb.position, movePoint.position, Speed * Time.deltaTime);
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        if (vertical == 1)
+        if (Vector3.Distance(Rb.position, movePoint.position) <= .01f)
         {
-            Rb.velocity = Vector2.up * (Speed * Time.deltaTime * 1000);
-            Anim.SetFloat("MoveX", 0);
-            Anim.SetFloat("MoveY", 1);
-        }
-        if (vertical == -1)
-        {
-            Rb.velocity = Vector2.down * (Speed * Time.deltaTime * 1000);
-            Anim.SetFloat("MoveX", 0);
-            Anim.SetFloat("MoveY", -1);
-        }
-        if (horizontal == 1)
-        {
-            Rb.velocity = Vector2.right * (Speed * Time.deltaTime * 1000);
-            Anim.SetFloat("MoveY", 0);
-            Anim.SetFloat("MoveX", 1);
-        }
-        if (horizontal == -1)
-        {
-            Rb.velocity = Vector2.left * (Speed * Time.deltaTime * 1000);
-            Anim.SetFloat("MoveY", 0);
-            Anim.SetFloat("MoveX", -1);
+            if (vertical == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 1f, 0f), .1f, whatStopsMovement))
+                    movePoint.position += new Vector3(0f, 1f, 0f);
+                Anim.SetFloat("MoveX", 0);
+                Anim.SetFloat("MoveY", 1);
+            }
+            else if (vertical == -1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .2f, whatStopsMovement))
+                    movePoint.position += new Vector3(0f, -1f, 0f);
+                Anim.SetFloat("MoveX", 0);
+                Anim.SetFloat("MoveY", -1);
+            }
+            else if (horizontal == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(1f, 0f, 0f), .1f, whatStopsMovement))
+                    movePoint.position += new Vector3(1f, 0f, 0f);
+                Anim.SetFloat("MoveY", 0);
+                Anim.SetFloat("MoveX", 1);
+            }
+            else if (horizontal == -1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-1f, 0f, 0f), .2f, whatStopsMovement))
+                    movePoint.position += new Vector3(-1f, 0f, 0f);
+                Anim.SetFloat("MoveY", 0);
+                Anim.SetFloat("MoveX", -1);
+            }
         }
     }
 
