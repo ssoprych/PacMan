4047ade7c06a74c21cf5450015c05b57d7a6837f
ssoprych <sebastian.soprych@gmail.com>
2023-03-25 16:26:04 +0100

    map layout changed and made a rule tile

--- Assets/_project/Scripts/Coin.cs
+++ Assets/_project/Scripts/Coin.cs
@@ -5,13 +5,6 @@ using UnityEngine.Tilemaps;
 
 public class Coin : MonoBehaviour
 {
-    public Tilemap tileMap;
-    public TileBase baseTile;
-    private Vector3Int position;
-    private Vector3Int positionTile;
-    private int _positionX;
-    private int _positionY;
-
     private void Update()
     {
         //if (Input.GetMouseButtonDown(0))
@@ -29,9 +22,7 @@ public class Coin : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PagMan"))
         {
-            Vector2 playerPosition = Camera.main.ScreenToWorldPoint(collision.gameObject.transform.position);
-            Vector3Int gridPosition = tileMap.WorldToCell(playerPosition);
-            tileMap.SetTile(gridPosition, baseTile);
+            Destroy(gameObject);
             GameManager.Instance.Score += 20;
         }
     }
