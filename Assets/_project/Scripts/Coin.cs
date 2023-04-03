bbab58c3a4eda43251bad66bfe9cf980cd32deb3
ssoprych <sebastian.soprych@gmail.com>
2023-04-03 21:09:24 +0200

    Eating coins

--- Assets/_project/Scripts/Coin.cs
+++ Assets/_project/Scripts/Coin.cs
@@ -5,21 +5,6 @@ using UnityEngine.Tilemaps;
 
 public class Coin : MonoBehaviour
 {
-    public Tilemap tileMap;
-    public TileBase baseTile;
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int gridPosition = tileMap.WorldToCell(mousePosition);
-
-            TileBase clickedTile = tileMap.GetTile(gridPosition);
-            tileMap.SetTile(gridPosition, baseTile);
-            print(clickedTile);
-        }
-
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PagMan"))
